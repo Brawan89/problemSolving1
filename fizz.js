05a68e114a729e5cedb6372ba154ebe6383b560c
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-08 23:14:50 +0300

    prblem solving

--- fizz.js
+++ fizz.js
@@ -1,3 +1,4 @@
+//1
 const fizzbuzz = (n) => {
 for (let i = 1; i <= n; i++) {
     
@@ -14,16 +15,14 @@ for (let i = 1; i <= n; i++) {
 }}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//2
+function isPalindrome(x) {
+   return x.split('').reverse().join('').toLowerCase() === x.toLowerCase() ? true : false
+ }
+ //or 
+ function isPalindrome(x) {
+    let splitted = x.split("");
+    let reversed = splitted.reverse("");
+    let joined = reversed.join("");
+    return joined.toLowerCase().replace('') == x.toLowerCase().replace('')
+   }
