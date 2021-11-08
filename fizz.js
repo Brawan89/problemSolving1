e13d4f429d5779a0c0f0b944a7bc6c463ed767ef
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-08 23:15:53 +0300

    prblem solving

--- fizz.js
+++ fizz.js
@@ -6,10 +6,10 @@ for (let i = 1; i <= n; i++) {
         console.log(i + ' fizzbuzz');
     }
     else if (i%3 === 0) {
-        console.log(i);
+        console.log(i + "Fizz");
     }
     else if (i%5 === 0) {
-        console.log(i);
+        console.log(i + "Buzz");
     }
 
 }}
